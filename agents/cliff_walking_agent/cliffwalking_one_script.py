b07347e5c02656908f5cb48afdfba6f82ff7e7ba
Juan Cruz Barsce <jbarsce@gmail.com>
2019-10-13 17:32:39 -0300

    Fixed bug

--- agents/cliff_walking_agent/cliffwalking_one_script.py
+++ agents/cliff_walking_agent/cliffwalking_one_script.py
@@ -7,55 +7,6 @@ import gym
 import numpy as np
 
 
-def run():
-    """
-    Runs the reinforcement learning agent with a given configuration.
-    """
-    # list that contains the amount of time-steps of the episode. It is used as a way to score the performance of
-    # the agent.
-    timesteps_of_episode = []
-    # list that contains the amount of reward given to the agent in each episode
-    reward_of_episode = []
-
-    for i_episode in range(episodes_to_run):
-        # an instance of an episode is run until it fails or until it reaches 200 time-steps
-
-        # resets the environment, obtaining the first state observation
-        state = env.reset()
-
-        episode_reward = 0
-        done = False
-        t = 0
-
-        while not done:
-
-            # Pick an action based on the current state
-            action = choose_action(state)
-            # Execute the action and get feedback
-            observation, reward, done, info = env.step(action)
-
-            if done:
-                reward = +1
-            elif reward == -1:
-                reward = 0
-
-            episode_reward += reward
-
-            next_state = observation
-
-            if not done:
-                learn(state, action, reward, next_state)
-                state = next_state
-            else:
-                learn(state, action, reward, next_state)
-                timesteps_of_episode = np.append(timesteps_of_episode, [int(t + 1)])
-                reward_of_episode = np.append(reward_of_episode, max(episode_reward, -100))
-
-            t += 1
-
-    return reward_of_episode.mean(), timesteps_of_episode, reward_of_episode
-
-
 def choose_action(state):
     """
     Chooses an action according to the learning previously performed
@@ -86,18 +37,62 @@ def learn(state, action, reward, next_state):
     Q-learning update:
     Q(s, a) += alpha * (reward(s,a) + max(Q(s') - Q(s,a))
     """
-    new_max_q = max([q.get((next_state, a), 0.0) for a in actions]) if next_state else 0
+    new_max_q = max([q.get((next_state, a), 0.0) for a in actions])
     old_value = q.get((state, action), 0.0)
 
     q[(state, action)] = old_value + alpha * (reward + gamma * new_max_q - old_value)
 
 
-# definimos sus híper-parámetros básicos
+def run():
+    """
+    Runs the reinforcement learning agent with a given configuration.
+    """
+    # list that contains the amount of time-steps of the episode. It is used as a way to score the performance of
+    # the agent.
+    timesteps_of_episode = []
+    # list that contains the amount of reward given to the agent in each episode
+    reward_of_episode = []
+
+    for i_episode in range(episodes_to_run):
+        # an instance of an episode is run until it fails or until it reaches 200 time-steps
+
+        # resets the environment, obtaining the first state observation
+        state = env.reset()
+
+        episode_reward = 0
+        done = False
+        t = 0
+
+        while not done:
+
+            # Pick an action based on the current state
+            action = choose_action(state)
+            # Execute the action and get feedback
+            observation, reward, done, info = env.step(action)
+
+            episode_reward += reward
+
+            next_state = observation
+
+            if not done:
+                learn(state, action, reward, next_state)
+                state = next_state
+            else:
+                learn(state, action, reward, next_state)
+                timesteps_of_episode = np.append(timesteps_of_episode, [int(t + 1)])
+                reward_of_episode = np.append(reward_of_episode, max(episode_reward, -100))
+
+            t += 1
+
+    return reward_of_episode.mean(), timesteps_of_episode, reward_of_episode
+
 
 q = {}
 
-alpha = 0.5
-gamma = 0.9
+# definimos sus híper-parámetros básicos
+
+alpha = 1
+gamma = 1
 epsilon = 0.1
 tau = 25
 
@@ -110,9 +105,9 @@ actions = range(env.action_space.n)
 random_state = np.random.RandomState(42)
 
 # se realiza la ejecución del agente
-avg_steps_per_episode, timesteps_of_episode, reward_of_episode = run()
+avg_steps_per_episode, timesteps_ep, reward_ep = run()
 
-episode_rewards = np.array(reward_of_episode)
+episode_rewards = np.array(reward_ep)
 
 
 # se suaviza la curva de convergencia
@@ -128,7 +123,7 @@ plt.show()
 # ---
 
 # se muestra la curva de aprendizaje de los pasos por episodio
-episode_steps = np.array(timesteps_of_episode)
+episode_steps = np.array(timesteps_ep)
 plt.plot(np.array(range(0, len(episode_steps))), episode_steps)
 plt.title('Pasos (timesteps) por episodio')
 plt.show()
@@ -167,7 +162,7 @@ for row in range(n_rows):
         q_value_matrix[row, column] = maximum_value
 
 # el valor del estado objetivo se asigna en 1 (reward recibido al llegar) para que se coloree de forma apropiada
-q_value_matrix[3, 11] = 1
+q_value_matrix[3, 11] = -1
 
 # se grafica la matriz de valor
 plt.imshow(q_value_matrix, cmap=plt.cm.RdYlGn)
